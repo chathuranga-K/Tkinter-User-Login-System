04c28b4d853d32b0e018d0d0b7f2772c82c08367
chathuranga-K <chathurang9at@gmail.com>
2026-05-23 16:32:02 +0530

    Created Datasets.py to store usernames & passowrds

---
+++ Datasets.py
@@ -0,0 +1,11 @@
+"""
+This file contains all the User login information & Passwords
+"""
+
+def save_info(login_username):
+    userinfo = {
+        login_username: {
+            'username' : '',
+            'password' : ''
+        }
+    }
